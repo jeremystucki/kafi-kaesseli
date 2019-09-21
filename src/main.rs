0c1d2e9fbf9304d98abd76be644c583cb4c9cee6
Jeremy Stucki <jeremy@myelin.ch>
2019-09-22 00:12:43 +0200

    Work on currency parser
    Co-authored-by: Stefanie Jäger <stefijaeger@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 #![feature(exclusive_range_pattern)]
 
 mod currency_formatter;
+mod currency_parser;
+mod message_handler;
 
 type Rappen = i32;
+
+enum MessageValidity {
+    Valid(MessageType),
+    Invalid,
+}
+
+enum MessageType {
+    Command(String),
+    Amount(Rappen),
+}
 
 fn main() {
     println!("Hello, world!");
