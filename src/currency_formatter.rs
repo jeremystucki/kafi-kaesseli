1e96a77fff232b55ad9aa6946704be77c3d4ec91
Jeremy Stucki <jeremy@myelin.ch>
2019-09-21 18:52:09 +0200

    Implement currency formatter
    Co-authored-by: Stefanie Jäger <stefijaeger@gmail.com>

---
+++ src/currency_formatter.rs
@@ -0,0 +1,92 @@
+use crate::Rappen;
+use std::ops::Add;
+
+fn format_whole_franken_amount(amount: Rappen) -> String {
+    let franken_amount = amount.abs() / 100;
+
+    // Only return "-" if the rappen amount is not zero
+    if amount != 0 && franken_amount == 0 {
+        String::from("-")
+    } else {
+        franken_amount.to_string()
+    }
+}
+
+fn format_rappen_amount(amount: Rappen) -> String {
+    let rappen_amount = amount.abs() % 100;
+
+    match rappen_amount {
+        0 => String::from("-"),
+        1..9 => format!("0{}", rappen_amount),
+        _ => rappen_amount.to_string(),
+    }
+}
+
+pub(crate) fn format_amount(amount: Rappen) -> String {
+    let prefix = if amount >= 0 { "" } else { "- " };
+    let formatted_franken = format_whole_franken_amount(amount);
+    let formatted_rappen = format_rappen_amount(amount);
+
+    format!("{}{}.{}", prefix, formatted_franken, formatted_rappen)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn formats_zero_correctly() {
+        let expected = String::from("0.-");
+        let actual = format_amount(0);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn formats_negative_one_hundred_correctly() {
+        let expected = String::from("- 1.-");
+        let actual = format_amount(-100);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn formats_negative_one_hundred_and_one_correctly() {
+        let expected = String::from("- 1.01");
+        let actual = format_amount(-101);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn formats_negative_ninety_nine_correctly() {
+        let expected = String::from("- -.99");
+        let actual = format_amount(-99);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn formats_one_hundred_correctly() {
+        let expected = String::from("1.-");
+        let actual = format_amount(100);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn formats_one_hundred_and_one_correctly() {
+        let expected = String::from("1.01");
+        let actual = format_amount(101);
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn formats_ninety_nine_correctly() {
+        let expected = String::from("-.99");
+        let actual = format_amount(99);
+
+        assert_eq!(expected, actual);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![feature(exclusive_range_pattern)]
+
+mod currency_formatter;
+
+type Rappen = i32;
+
+fn main() {
+    println!("Hello, world!");
+}
