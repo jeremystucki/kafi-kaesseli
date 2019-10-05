9b6f87a9295ff992ecefbd91ad8cfe9085fd5b90
Jeremy Stucki <jeremy@myelin.ch>
2019-10-05 13:05:17 +0200

    Create users migration

--- src/schema.rs
+++ src/schema.rs
@@ -6,3 +6,15 @@ table! {
         price -> Integer,
     }
 }
+
+table! {
+    users (id) {
+        id -> Integer,
+        name -> Text,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    products,
+    users,
+);
