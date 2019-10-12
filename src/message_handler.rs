f7c19621a00926a35a61ae12b809541f76025b45
Jeremy Stucki <jeremy@myelin.ch>
2019-10-12 16:40:45 +0200

    Work on message handler

--- src/message_handler.rs
+++ src/message_handler.rs
@@ -1,7 +1,9 @@
 use crate::currency_formatter::CurrencyFormatter;
 use crate::message_router::MessageRouter;
-use crate::{Message, Response};
-use diesel::SqliteConnection;
+use crate::models::Product;
+use crate::schema::products;
+use crate::{Command, Message, MessageAction, Response};
+use diesel::{RunQueryDsl, SqliteConnection};
 
 pub trait MessageHandler {
     fn handle_message(&self, message: Message) -> Vec<Response>;
@@ -13,9 +15,67 @@ pub struct MessageHandlerImpl<'a> {
     currency_formatter: Box<dyn CurrencyFormatter>,
 }
 
+impl MessageHandlerImpl<'_> {
+    fn handle_message_action(&self, message_action: MessageAction) -> Vec<Response> {
+        match message_action {
+            MessageAction::Command(command) => self.handle_command(command),
+            MessageAction::Product(product) => unimplemented!(),
+            MessageAction::Amount(amount) => unimplemented!(),
+        }
+    }
+
+    fn handle_command(&self, command: Command) -> Vec<Response> {
+        match command {
+            Command::ListAvailableItems => self
+                .get_available_products()
+                .map(|products| {
+                    vec![Response {
+                        contents: self.format_available_products(products),
+                    }]
+                })
+                .unwrap_or_else(|_| {
+                    vec![Response {
+                        contents: "Internal error (2)".to_string(),
+                    }]
+                }),
+            Command::GetCurrentStats => unimplemented!(),
+        }
+    }
+
+    fn get_available_products(&self) -> Result<Vec<Product>, ()> {
+        products::dsl::products
+            .load::<Product>(self.database_connection)
+            .map_err(|_| ())
+    }
+
+    fn format_available_products(&self, available_products: Vec<Product>) -> String {
+        let message_header = "Available products:";
+        let message_body = available_products
+            .into_iter()
+            .map(|product| {
+                format!(
+                    "- /{} - {} ({})",
+                    product.identifier, product.name, product.price
+                )
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        format!("{}\n{}", message_header, message_body)
+    }
+}
+
 impl MessageHandler for MessageHandlerImpl<'_> {
     fn handle_message(&self, message: Message) -> Vec<Response> {
-        unimplemented!()
+        match self.message_router.route_message(&message) {
+            Err(_) => vec![Response {
+                contents: "Internal error (1)".to_string(),
+            }],
+            Ok(None) => vec![Response {
+                contents: "Invalid input".to_string(),
+            }],
+            Ok(Some(message_action)) => self.handle_message_action(message_action),
+        }
     }
 }
 
